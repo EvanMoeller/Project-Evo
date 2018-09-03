ba38b62ada644d65c3347352b69de43d0180b255
Evan Moeller <emoelle2@binghamton.edu>
2018-09-03 17:14:46 -0400

    Simple navigation bar

--- src/server.py
+++ src/server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/login")
+def main():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
