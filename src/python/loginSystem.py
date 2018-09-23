72203581f76cc4790312f19cfabaa662501b9a9a
Evan Moeller <emoelle2@binghamton.edu>
2018-09-23 13:57:53 -0400

    Fixed linux server

--- src/python/loginSystem.py
+++ src/python/loginSystem.py
@@ -3,3 +3,11 @@ import cgi
 form = cgi.FieldStorage()
 username = form.getvalue('username')
 password = form.getvalue('password')
+
+
+#Test code to check input form
+def main()
+{
+    print("Username: " + username)
+    print("Password: " + password)
+}
